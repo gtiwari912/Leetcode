b1408f474f4c756768dcb172e212319563d1ecf8
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-06-09 14:34:54 +0530

    Time: 2 ms (67.93%), Space: 49.7 MB (64.98%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,34 +1,24 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        for(int i = 0; i<numbers.length; i++){
-            int curNum = numbers[i];
-            int req = target-curNum;
-            int secondIndex = binSearch(numbers, req);
-            if(secondIndex != -1 && secondIndex != i+1){
+        int start = 0;
+        int end = numbers.length -1;
+        while(start<end){
+            int num1 = numbers[start];
+            int num2 = numbers[end];
+            if(num1 + num2 == target){
                 int[] ans = new int[2];
-                ans[0] = i+1;
-                ans[1] = secondIndex;
-                Arrays.sort(ans);
+                ans[0] = start+1;
+                ans[1] = end+1;
                 return ans;
+            }
+            else if(num1 + num2 < target){
+                start++;
+            }
+            else{
+                end--;
             }
         }
         
         return new int[2];
-    }
-    
-    private int binSearch(int[] arr, int target){
-        int start = 0;
-        int end = arr.length -1;
-        while(start<=end){
-            int mid = (start+end)/2;
-            if(arr[mid]==target)
-                return mid+1;
-            if(arr[mid]<target){
-                start = mid+1;
-            }
-            else
-                end = mid-1;
-        }
-        return -1;
     }
 }

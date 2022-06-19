f472bfc0be6000c14d380ab6a1e7826165372c76
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-06-19 23:42:54 +0530

    Time: 7 ms (43.65%), Space: 49.8 MB (51.52%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,24 +1,35 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        int limit = 0;
-        if(n%3==0){
-            limit = n/3+1;
-        }
-        else{
-            limit = n/3 + 1;
-        }
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int cur: nums){
-            map.put(cur, map.getOrDefault(cur, 0) +1);
-        }
-        
-        List<Integer> ans = new ArrayList<>();
-        for(int cur: map.keySet()){
-            if(map.get(cur) >= limit)
+        int limit = n/3 + 1;
+        if(n==1 || n==2){
+            List<Integer> ans = new ArrayList<>();
+            HashSet<Integer> ansSet = new HashSet<>();
+            for(int cur: nums)
+                ansSet.add(cur);
+            for(int cur: ansSet)
                 ans.add(cur);
+            return ans;
         }
-        
+        Arrays.sort(nums);
+        int count = 1;
+        int prev = nums[0];
+        List<Integer> ans = new ArrayList<>();
+        for(int i=1; i<n; i++){
+            int cur = nums[i];
+            if(cur == prev){
+                count++;
+                if(count>=limit){
+                    if(ans.isEmpty() || ans.get(ans.size()-1) != cur){
+                        ans.add(cur);
+                    }
+                }
+            }
+            else{
+                count = 1;
+                prev = cur;
+            }
+        }
         
         return ans;
         

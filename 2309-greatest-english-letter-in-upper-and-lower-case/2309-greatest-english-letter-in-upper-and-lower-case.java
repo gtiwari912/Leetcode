1d6a85fa63bbbfeba381b87a7d4c42dcf64cc2b1
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-06-19 11:25:17 +0530

    Time: 11 ms (50.00%), Space: 43.3 MB (25.00%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
@@ -3,20 +3,22 @@ class Solution {
         HashSet<Integer> upperSet = new HashSet<>();
         HashSet<Integer> lowerSet = new HashSet<>();
         for(char c: s.toCharArray()){
-            if(c>='a' && c<='z')
+            if(c>='a' && c<='z') // c is lowercase
                 lowerSet.add(c-'a');
-            else
+            else // c is uppercase
                 upperSet.add(c-'A');
         }
         
         int max = -1;
-        for(int i = 0; i<26; i++){
+		int TOTAL_ALPHABETS = 26;
+        for(int i = 0; i < TOTAL_ALPHABETS; i++){
             if(upperSet.contains(i) && lowerSet.contains(i))
                 max = i;
         }
-        
+		
         if(max==-1)
             return "";
+			
         char ansChar = (char)((int)'A'+max);
         return ""+ansChar;
     }

d1b5d5460ece948e295fa38c9fa510b9bdec39eb
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-06-23 19:48:05 +0530

    Time: 274 ms (26.72%), Space: 46.7 MB (87.50%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,57 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
         HashSet<List<Integer>> ansSet = new HashSet<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int cur: nums){
-            map.put(cur, map.getOrDefault(cur, 0)+1);
-        }
+        Arrays.sort(nums);
+        int n = nums.length;
         for(int i = 0; i<n; i++){
-            int firstNum = nums[i];
-            for(int j = i+1; j<n; j++){
-                int secondNum = nums[j];
-                int req = 0 - firstNum - secondNum;
-                if(map.containsKey(req)){
-                    if(firstNum != req && secondNum != req){
-                        List<Integer> now = new ArrayList<>()
-                        {{
-                            add(firstNum);
-                            add(secondNum);
-                            add(req);
-                        }};
-                        Collections.sort(now);
-                        ansSet.add(now);
-                    }
-                    else if(firstNum==secondNum && req==firstNum){
-                        if(map.get(req)>2){
-                            List<Integer> now = new ArrayList<>()
-                            {{
-                                add(firstNum);
-                                add(secondNum);
-                                add(req);
-                            }};
-                            Collections.sort(now);
-                            ansSet.add(now);
-                        }
-                    }
-                    else{
-                        if(map.get(req)>1){
-                            List<Integer> now = new ArrayList<>()
-                            {{
-                                add(firstNum);
-                                add(secondNum);
-                                add(req);
-                            }};
-                            Collections.sort(now);
-                            ansSet.add(now);
-                        }
-                    }
+            int num1 = nums[i];
+            int target = -num1;
+            int start = i+1;
+            int end = n-1;
+            while(start<end){
+                if(nums[start] + nums[end] == target){
+                    ArrayList<Integer> now = new ArrayList<>();
+                    now.add(num1);
+                    now.add(nums[start]);
+                    now.add(nums[end]);
+                    ansSet.add(now);
+                    start++; end--;
+                }
+                else if(nums[start] + nums[end] < target){
+                    start++;
+                }
+                else{
+                    end--;
                 }
             }
         }
-        for(List<Integer> na: ansSet)
-            ans.add(na);
+        
+        for(List<Integer> now: ansSet)
+            ans.add(now);
+        
         return ans;
     }
 }

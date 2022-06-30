e74e39e6d35c4fe53d0cf3dde00e48153c2c9248
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-06-30 15:42:03 +0530

    Time: 18 ms, Memory: 47.6 MB - LeetHub

---
+++ search-for-a-range/search-for-a-range.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        int n = nums.length;
+        if(n==0){
+            return ans;
+        }
+        if(n==1){
+            if(nums[0] == target){
+                ans[0] = 0; ans[1] = 0;
+                return ans;
+            }
+            return ans;
+        }
+        int one = findFirst(nums, 0, n-1, target);
+        int two = findLast(nums, 0, n-1, target);
+        ans[0] = one;
+        ans[1] = two;
+        return ans;
+    }
+    
+    private int findFirst(int[] nums, int left, int right, int target){
+        if(right<left)
+            return -1;
+        if(left==right){
+            if(nums[right] == target)
+                return right;
+            return -1;
+        }
+        int mid = (left+right)/2;
+        System.out.println("FF l:"+left+" r:"+right+" m:"+mid);
+        if(nums[mid]==target){
+            return findFirst(nums, left, mid, target);
+        }
+        else if(nums[mid]>target){
+            return findFirst(nums, left, mid-1, target);
+        }
+        else{
+            return findFirst(nums, mid+1, right, target);
+        }
+    }
+    
+    private int findLast(int[] nums, int left, int right, int target){
+        if(left==right){
+            if(nums[right] == target)
+                return right;
+            return -1;
+        }
+        int mid = (left+right)/2 + 1;
+        if(nums[mid]<=target){
+            return findLast(nums, mid, right, target);
+        }
+        else{
+            return  findLast(nums, left, mid-1, target);
+        }
+    }
+}

f5c47044fbc2d818cd1b00814320936b2fedee2d
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-07-01 15:30:14 +0530

    Time: 1 ms (49.76%), Space: 47.8 MB (62.15%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
@@ -4,7 +4,7 @@ class Solution {
         int right = letters.length-1;
         while(left<=right){
             int mid = (left+right)/2;
-            System.out.println("l:"+left+" r:"+right+" m:"+mid);
+            // System.out.println("l:"+left+" r:"+right+" m:"+mid);
             if(letters[mid] == target){
                 left = mid+1;
                 // break;
@@ -16,7 +16,7 @@ class Solution {
                 right = mid-1;
             }
         }
-        System.out.println("LEFT POSITON: "+left);
+        // System.out.println("LEFT POSITON: "+left);
         if(left==letters.length)
             return letters[0];
         return letters[left];

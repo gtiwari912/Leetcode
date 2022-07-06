3e1b392b0a00d0edb5db4e6866ddd0669913f28b
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-07-06 16:02:24 +0530

    Time: 23 ms (87.98%), Space: 61.2 MB (83.51%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,22 +1,46 @@
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         HashSet<Integer> set = new HashSet<Integer>();
+//         for(int i : nums)
+//             set.add(i);
+//         int longestStreak = 0;
+//         int curStreak = 0;
+//         for(int i: nums){
+//             if(!set.contains(i-1)){
+//                 int cur = i+1;
+//                 curStreak = 1;
+//                 while(set.contains(cur)){
+//                     cur++;
+//                     curStreak++;
+//                 }
+//                 longestStreak = Math.max(curStreak, longestStreak);
+//             }
+//         }
+        
+//         return longestStreak;
+//     }
+// }
 class Solution {
     public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet<Integer>();
-        for(int i : nums)
-            set.add(i);
+        HashSet<Integer> set = new HashSet<>();
+        for(int cur: nums)
+            set.add(cur);
+        
+        
         int longestStreak = 0;
-        int curStreak = 0;
-        for(int i: nums){
-            if(!set.contains(i-1)){
-                int cur = i+1;
-                curStreak = 1;
+        for(int cur: set){
+            if(!set.contains(cur-1)){
+                int curStreak = 1;
+                cur++;
                 while(set.contains(cur)){
-                    cur++;
                     curStreak++;
+                    cur++;
                 }
-                longestStreak = Math.max(curStreak, longestStreak);
+                longestStreak = Math.max(longestStreak, curStreak);
             }
         }
         
         return longestStreak;
+        
     }
 }

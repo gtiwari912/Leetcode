cad78c4c38bbc0e931b4a7918961c7100c7b5f93
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-07-06 16:04:42 +0530

    Time: 48 ms (68.31%), Space: 79.5 MB (42.27%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,46 +1,22 @@
-// class Solution {
-//     public int longestConsecutive(int[] nums) {
-//         HashSet<Integer> set = new HashSet<Integer>();
-//         for(int i : nums)
-//             set.add(i);
-//         int longestStreak = 0;
-//         int curStreak = 0;
-//         for(int i: nums){
-//             if(!set.contains(i-1)){
-//                 int cur = i+1;
-//                 curStreak = 1;
-//                 while(set.contains(cur)){
-//                     cur++;
-//                     curStreak++;
-//                 }
-//                 longestStreak = Math.max(curStreak, longestStreak);
-//             }
-//         }
-        
-//         return longestStreak;
-//     }
-// }
 class Solution {
     public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int cur: nums)
-            set.add(cur);
-        
-        
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i : nums)
+            set.add(i);
         int longestStreak = 0;
-        for(int cur: set){
-            if(!set.contains(cur-1)){
-                int curStreak = 1;
-                cur++;
+        int curStreak = 0;
+        for(int i: set){
+            if(!set.contains(i-1)){
+                int cur = i+1;
+                curStreak = 1;
                 while(set.contains(cur)){
-                    curStreak++;
                     cur++;
+                    curStreak++;
                 }
-                longestStreak = Math.max(longestStreak, curStreak);
+                longestStreak = Math.max(curStreak, longestStreak);
             }
         }
         
         return longestStreak;
-        
     }
 }

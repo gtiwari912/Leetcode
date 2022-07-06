f920887d2f49db098795ea481e7ebbe9200355b7
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-07-06 17:57:12 +0530

    Time: 85 ms (47.82%), Space: 81.5 MB (36.36%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,22 +1,60 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet<Integer>();
-        for(int i : nums)
-            set.add(i);
-        int longestStreak = 0;
-        int curStreak = 0;
-        for(int i: set){
-            if(!set.contains(i-1)){
-                int cur = i+1;
-                curStreak = 1;
-                while(set.contains(cur)){
-                    cur++;
-                    curStreak++;
-                }
-                longestStreak = Math.max(curStreak, longestStreak);
+        int n = nums.length;
+        UnionFind uf = new UnionFind(n);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            if(map.containsKey(nums[i]))
+                continue;
+            if(map.containsKey(nums[i]+1)){
+                uf.union(i, map.get(nums[i] + 1));
+            }
+            if(map.containsKey(nums[i] - 1 ))
+                uf.union(i, map.get(nums[i] - 1 ));
+            map.put(nums[i], i);
+        }
+        
+        return uf.getLargestComponentSize();
+    }
+}
+
+class UnionFind{
+    int[] parent;
+    int[] size;
+    UnionFind(int n){
+        parent = new int[n];
+        size = new int[n];
+        for(int i = 0; i<n; i++){
+            parent[i]= i;
+            size[i] = 1;
+        }
+    }
+    
+    public void union(int v1, int v2){
+        int root1 = find(v1);
+        int root2 = find(v2);
+        if(root1 != root2){
+            parent[root1] = root2;
+            size[root2] += size[root1];
+        }
+    }
+    
+    public int find(int v){
+        if(parent[v] == v)
+            return v;
+        int ans = find(parent[v]);
+        parent[v] = ans;
+        return ans;
+    }
+    
+    public int getLargestComponentSize(){
+        int maxSize = 0;
+        for(int i = 0; i<parent.length; i++){
+            if(parent[i] == i){
+                maxSize = Math.max(maxSize, size[i]);
             }
         }
         
-        return longestStreak;
+        return maxSize;
     }
 }

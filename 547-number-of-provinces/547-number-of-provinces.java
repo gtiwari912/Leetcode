b239ec2bd28158e8af0f5c2a2fe07370b706e34c
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-07-08 01:23:10 +0530

    Time: 11 ms (8.81%), Space: 54.2 MB (35.37%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,72 +1,39 @@
 class Solution {
-    public int findCircleNum(int[][] graph) {
-        int n = graph.length;
-        UnionFind uf = new UnionFind(n);
-        for(int i = 0 ;i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(graph[i][j] == 1){
-                    uf.union(i, j);
+    int isConnected[][];
+    int n;
+    public int findCircleNum(int[][] isConneted) {
+        int ans = 0;
+        this.isConnected = isConneted;
+        n = isConnected.length;
+        int m = isConnected[0].length;
+        for(int i = 0; i<n; i++){
+            boolean sawNew = false;
+            for(int j = 0; j<m; j++){
+                if(isConnected[i][j]==1){
+                    sawNew = true;
+                    isConnected[i][j] = -1;
+                    if(i!=j)
+                        visitNeighbours(j);
+                }
+            }
+            if(sawNew)
+                    ans++;
+        }
+        
+        return ans;
+    }
+    
+    private void visitNeighbours(int r){
+        // for(int[] cur: isConnected)
+            // System.out.println(Arrays.toString(cur));
+        // System.out.println("Visiting row: "+r);
+        for(int i = 0; i<n; i++){
+            if(isConnected[r][i]==1){
+                isConnected[r][i] = -1;
+                if(r!=i){
+                    visitNeighbours(i);
                 }
             }
         }
-        
-        return uf.components();
     }
-}
-
-class UnionFind{
-    int num;
-    int numComponents;
-    int[] rank;
-    int[] parent;
-
-    UnionFind(int n){
-        num = n; numComponents = n;
-        rank = new int[n];
-        Arrays.fill(rank, 0);
-        parent = new int[n];
-        for(int i = 0; i<n; i++)
-            parent[i] = i;
-    }
-    
-    int size(){return num;}
-    
-    int components(){return numComponents;}
-    
-    int find(int p){
-        while(p != parent[p]){
-            parent[p] = parent[parent[p]];
-            p = parent[p];
-        }
-        return p;
-    }
-
-    public boolean isCycle(int[][] edgeList){
-        for(int[] edge : edgeList){
-            int v1 = edge[0];
-            int v2 = edge[1];
-            int root1 = find(v1);
-            int root2 = find(v2);
-            if(root1 == root2)
-                return true;
-            union(root1, root2);
-        }
-        return false;
-    }
-    
-    public void union(int p, int q){
-        int rootp = find(p); int rootq = find(q);
-        if(rootp == rootq) return;
-        if(rank[rootp] < rank[rootq]) parent[rootp] = rootq;
-        else{
-            parent[rootq] = rootp;
-            if(rank[rootp] == rank[rootq]){rank[rootp]++;}
-        }
-        numComponents--;
-    }
-    
-    public boolean connected(int p, int q){
-        return find(p) == find(q);
-    }
-    
 }

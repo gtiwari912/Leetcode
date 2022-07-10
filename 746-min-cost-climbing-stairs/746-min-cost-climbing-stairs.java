b141414b9e759c222502f8d15285b3e80b7c027e
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-07-10 18:07:45 +0530

    Time: 7 ms (5.65%), Space: 46.2 MB (5.07%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,9 +1,9 @@
 class Solution {
-    int[] dp;
+    HashMap<Integer, Integer> dp;
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        dp = new int[n];
-        Arrays.fill(dp, -1);
+        dp = new HashMap<>();
+        // Arrays.fill(dp, -1);
         int startFrom0 = solve(cost, 0, 0);
         int startFrom1 = solve(cost, 0, 1);
         return Math.min(startFrom0, startFrom1);
@@ -13,13 +13,13 @@ class Solution {
         
         if(curPos>=cost.length)
             return curVal;
-        if(dp[curPos] != -1)
-            return curVal+dp[curPos];
+        if(dp.containsKey(curPos))
+            return curVal+dp.get(curPos);
         int cur = cost[curPos];
         int ans = Math.min(  solve(cost, curVal+cur, curPos+1) ,
                           solve(cost, curVal+cur, curPos+2 )
                        );
-        dp[curPos] = ans - curVal;
+        dp.put(curPos, ans - curVal);
         return ans;
         
     }

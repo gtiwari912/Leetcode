9cf0df6307a52043a16f079894df1862835ef465
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-08-07 10:42:08 +0530

    Time: 11 ms (5.50%), Space: 45.7 MB (7.52%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<int[]> stack = new Stack<>();
+        for(int i = 0; i<n; i++){
+            int ele = prices[i];
+            while(!stack.isEmpty() && stack.peek()[0]>=ele){
+                map.put(stack.pop()[1], ele );
+            }
+            int[] arr = {ele, i};
+            stack.add(arr);
+        }
+        
+        for(int i= 0; i<n; i++)
+        {
+            prices[i] = map.containsKey(i)? prices[i]-map.get(i) : prices[i];
+        }
+        
+        return prices;
+    }
+}

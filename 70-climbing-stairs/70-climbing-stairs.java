88d7b113e7dea7783ee3d812664e5528954381a4
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-08-14 15:54:51 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (67.14%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -17,17 +17,89 @@
 //     }
 // }
 
-// Dynamic Programming - Memorization/TopDown Approach;
-class Solution {
-    public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return recurse(n, dp);
-    }
+// Dynamic Programming - Memorization/TopDown Approach; //Accepted
+//Time: O(N)  Space: O(N) + O(N)
+// class Solution {
+//     public int climbStairs(int n) {
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp, -1);
+//         return recurse(n, dp);
+//     }
     
-    private int recurse(int n, int[] dp){
-        if(n<=1) return 1;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = recurse(n-1, dp) + recurse(n-2, dp);
+//     private int recurse(int n, int[] dp){
+//         if(n<=1) return 1;
+//         if(dp[n] != -1) return dp[n];
+//         return dp[n] = recurse(n-1, dp) + recurse(n-2, dp);
+//     }
+// }
+
+
+
+// Dynamic Programming - Tabulation/BottumUp;
+// Time: O(N) Space: O(N)
+class Solution{
+    public int climbStairs(int n){
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

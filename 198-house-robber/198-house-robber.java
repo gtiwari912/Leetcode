533de3a6b44ce9889c9c7092f948a962aed3d542
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-08-15 22:47:17 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (71.18%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,17 +1,37 @@
 class Solution {
-    public int rob(int[] nums) {
+    // BottomUp tabulation
+    // public int rob(int[] nums) {
+    //     int n = nums.length;
+    //     if(n==1)
+    //         return nums[0];
+    //     int[] dp = new int[n];
+    //     dp[0] = nums[0];
+    //     dp[1] = Math.max(nums[0], nums[1]);
+    //     for(int i = 2; i<n; i++){
+    //         dp[i]  = Math.max(
+    //             dp[i-2]+nums[i],
+    //             dp[i-1]
+    //         );
+    //     }
+    //     return dp[n-1];
+    // }
+    
+    // TopDown 
+    public int rob(int[] nums){
         int n = nums.length;
-        if(n==1)
-            return nums[0];
         int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for(int i = 2; i<n; i++){
-            dp[i]  = Math.max(
-                dp[i-2]+nums[i],
-                dp[i-1]
-            );
-        }
-        return dp[n-1];
+        Arrays.fill(dp, -1);
+        return rob(nums, dp, n-1);
+    }
+    
+    private int rob(int[] nums, int[] dp, int i){
+        if(dp[i] != -1)
+            return dp[i];
+        if(i==0) return nums[0];
+        if(i==1) return Math.max(nums[0], nums[1]);
+        return dp[i]  = Math.max(
+            rob(nums, dp, i-2) + nums[i], 
+            rob(nums, dp, i-1)
+        );
     }
 }

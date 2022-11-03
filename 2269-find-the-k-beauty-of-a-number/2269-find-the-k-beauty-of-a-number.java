d1a91652d3c568dc0bbb58158b6d3c1e02e76694
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-11-03 17:13:06 +0530

    Time: 5 ms (15.26%), Space: 39.5 MB (86.17%) - LeetHub

---
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int divisorSubstrings(int a, int k) {
+        String num = a+"";
+        int  ans = 0;
+        int n = num.length();
+        for(int i = 0; i<=n-k; i++){
+            String window = num.substring(i, i + k);
+            System.out.println("Cur Window is: "+window);
+            int noOnWindow =  Integer.parseInt(window);  //"333" -> 333
+            if(noOnWindow != 0 && a % noOnWindow == 0) ans++;
+        }
+        
+        return ans;
+        
+    }
+}
+
+
+
+
+// 240 ........... 24
+
+
+
+
+
+
+
+
+
+// str = "L I O N";
+//        0 1 2 3
+// str = str.substring(0,3);

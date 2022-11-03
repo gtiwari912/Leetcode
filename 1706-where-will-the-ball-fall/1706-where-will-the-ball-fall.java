916c7b7d1fa37567a946ea853b216ab50e69b0fa
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-11-04 00:13:59 +0530

    Time: 33 ms (5.41%), Space: 55.2 MB (7.85%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -2,8 +2,10 @@ class Solution {
     int[][] grid;
     int n;
     int m;
+    HashMap<String, Integer> map;
     public int[] findBall(int[][] grid) {
         this.grid = grid;
+        map = new HashMap<>();
         this.n = grid.length;
         this.m = grid[0].length;
         int[] ans = new int[m];
@@ -14,11 +16,15 @@ class Solution {
     }
     
     public int canReachEnd(int x, int y){
+        String str = x+","+y;
+        if(map.containsKey(str)) return map.get(str);
         if(x>=n) return y;
         if((grid[x][y]==1 && y==m-1) || (grid[x][y]==-1 && y==0) 
           || (grid[x][y]==1 && grid[x][y+1]==-1) || (grid[x][y]==-1 && grid[x][y-1]==1))
             return -1;
-        return (grid[x][y]==1)? canReachEnd(x+1, y+1) : canReachEnd(x+1, y-1);
+        int ans = (grid[x][y]==1)? canReachEnd(x+1, y+1) : canReachEnd(x+1, y-1);
+        map.put(str, ans);
+        return ans;
     }
     
     

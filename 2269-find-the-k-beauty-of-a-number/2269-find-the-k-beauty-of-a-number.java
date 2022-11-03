84cbe0087b5aea9e1417db86fbcaae64af0b1de2
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2022-11-03 17:13:32 +0530

    Time: 2 ms (46.54%), Space: 40.9 MB (55.65%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -5,7 +5,7 @@ class Solution {
         int n = num.length();
         for(int i = 0; i<=n-k; i++){
             String window = num.substring(i, i + k);
-            System.out.println("Cur Window is: "+window);
+            // System.out.println("Cur Window is: "+window);
             int noOnWindow =  Integer.parseInt(window);  //"333" -> 333
             if(noOnWindow != 0 && a % noOnWindow == 0) ans++;
         }

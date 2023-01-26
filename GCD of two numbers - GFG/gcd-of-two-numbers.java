2e058f0b1a944c3b5f1d80f14f2f805edd50e19f
Gaurav Tiwari <67228014+gtiwari912@users.noreply.github.com>
2023-01-26 15:24:52 +0530

    Added solution - LeetHub

--- GCD of two numbers - GFG/gcd-of-two-numbers.java
+++ GCD of two numbers - GFG/gcd-of-two-numbers.java
@@ -30,7 +30,7 @@ class Solution
         //code here
         if(A==0) return B;
         if(B==0) return A;
-        if(A>B) return gcd(A-B, B);
-        else return gcd(B-A, A);
+        if(A>B) return gcd(A%B, B);
+        else return gcd(B%A, A);
     } 
 }

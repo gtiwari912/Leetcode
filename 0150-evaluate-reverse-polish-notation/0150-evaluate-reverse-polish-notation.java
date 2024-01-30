a7c36196307e4bda4eff9b64cac19b9bd217c896
gtiwari912 <67228014+gtiwari912@users.noreply.github.com>
2024-01-30 18:22:43 +0530

    Time: 43 ms (5.00%), Space: 45.5 MB (5.03%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        int ans = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(String str: tokens){
+            if(str.equals("+")){
+                int one = stack.pop();
+                int two = stack.pop();
+                stack.add(one+two);
+                System.out.println("adding "+one+" and "+two);
+            }
+            else if(str.equals("-")){
+                int one = stack.pop();
+                int two = stack.pop();
+                stack.add(two-one);
+                System.out.println("Subtracting "+one+" and "+two);
+            }
+            else if(str.equals("/")){
+                int one = stack.pop();
+                int two = stack.pop();
+                stack.add(two/one);
+                System.out.println("Dividing "+one+" and "+two);
+            }
+            else if(str.equals("*")){
+                int one = stack.pop();
+                int two = stack.pop();
+                stack.add(one*two);
+                System.out.println("Multiplying "+one+" and "+two);
+            }
+            else{
+                int num = Integer.parseInt(str);
+                stack.add(num);
+            }
+        }
+        System.out.println(stack);
+        return stack.pop();
+    }
+}
